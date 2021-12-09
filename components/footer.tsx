437593fe97e230782c87d9fbf6176ae5f29130cb
Jason Long <jason@jasonlong.me>
2021-12-09 16:41:51 -0500

    Extract footer into component

---
+++ components/footer.tsx
@@ -0,0 +1,33 @@
+import { GithubLogo, HeartFilledIcon } from '@/components/icons'
+import * as React from 'react'
+
+export function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-between gap-4 pb-20 text-sm md:flex-row text-secondary pt-28">
+      <div className="inline-flex items-center gap-1 text-sm">
+        <span>Made with</span>
+        <HeartFilledIcon className="w-4 h-4" />
+        <span>
+          by{' '}
+          <a
+            href="https://planetscale.com"
+            target="_blank"
+            rel="noreferrer"
+            className="transition-colors text-secondary hover:text-primary"
+          >
+            PlanetScale
+          </a>
+        </span>
+      </div>
+      <a
+        href="https://github.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="inline-flex items-center gap-2 text-sm transition-colors text-secondary hover:text-primary"
+      >
+        <GithubLogo className="w-4 h-4" />
+        <span>Version 1.0.0</span>
+      </a>
+    </footer>
+  )
+}
