f7cee7356fc5e71f0c07878949936436fb62d891
Bogdan Soare <bgdn.sr@gmail.com>
2022-01-06 17:08:47 +0200

    Add session verification for sign cloudinary endpoint

--- pages/api/sign-cloudinary.ts
+++ pages/api/sign-cloudinary.ts
@@ -1,5 +1,7 @@
+import { authOptions } from '@/lib/auth'
 import { v2 as cloudinary } from 'cloudinary'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth/next'
 
 type Data = {
   timestamp: number
@@ -25,13 +27,19 @@ const apiSecret = cloudinary.config().api_secret!
 const apiKey = cloudinary.config().api_key!
 const folder = 'flux'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
   if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST'])
     return res.status(405).json({ message: `Method ${req.method} Not Allowed` })
+  }
+
+  const session = await getServerSession({ req, res }, authOptions)
+
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized' })
   }
 
   const timestamp = Math.round(new Date().getTime() / 1000)
