a2414bcba739315b1f7a7358b67e9f3d890252d5
rimas silkaitis <neovintage@gmail.com>
2022-03-17 11:04:44 -0700

    remove some stuff

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,23 +5,8 @@ import packageJson from '../package.json'
 export function Footer() {
   return (
     <footer className="flex flex-col items-center justify-between gap-2 text-sm md:gap-4 md:flex-row text-secondary">
-      <div className="inline-flex items-center gap-1 text-sm">
-        <span>Made with</span>
-        <HeartFilledIcon className="w-4 h-4" />
-        <span>
-          by{' '}
-          <a
-            href="https://planetscale.com"
-            target="_blank"
-            rel="noreferrer"
-            className="transition-colors text-secondary hover:text-primary"
-          >
-            PlanetScale
-          </a>
-        </span>
-      </div>
       <a
-        href="https://github.com/planetscale/beam"
+        href="https://github.com/meroxa/beam"
         target="_blank"
         rel="noreferrer"
         className="inline-flex items-center gap-2 text-sm transition-colors text-secondary hover:text-primary"
