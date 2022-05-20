81a75fd937a5f404c85dd3395a49a560df32511c
David Graham <david.malcom.graham@gmail.com>
2022-05-20 15:47:30 -0600

    Replace comment count subquery with array length
    The comments are loaded for display on the post page so we can skip the
    extra count subquery and use the array size instead.

--- server/routers/post.ts
+++ server/routers/post.ts
@@ -125,11 +125,6 @@ export const postRouter = createProtectedRouter()
               },
             },
           },
-          _count: {
-            select: {
-              comments: true,
-            },
-          },
         },
       })
 
