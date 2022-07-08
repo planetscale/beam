3b1260a7a52a8489d1565c73b0995a0f9c9f7fa0
Johannes Nicolai <jonico@planetscale.com>
2022-07-08 15:14:03 +0000

    Adding seeding logic with pscale database restore

--- .pscale/cli-helper-scripts/use-pscale-docker-image.sh
+++ .pscale/cli-helper-scripts/use-pscale-docker-image.sh
@@ -3,13 +3,11 @@ mkdir -p $HOME/.config/planetscale
 
 function pscale {
     local tty="-t"
-    local non_interactive=""
     local command=""
     
-    # if first arg equals shell, and we are getting input piped in we have to turn off pseudo-tty and set PSCALE_ALLOW_NONINTERACTIVE_SHELL=true
+    # if first arg equals shell, and we are getting input piped in we have to turn off pseudo-tty
     if [ "$1" = "shell" ] &&  ! [[ -t 0 ]]; then
         tty=""
-        non_interactive="-e PSCALE_ALLOW_NONINTERACTIVE_SHELL=true"
     fi
 
     # if script is run in CI and it is not the auth command, we have to turn off pseudo-tty
@@ -22,7 +20,7 @@ function pscale {
         command="`which pscale` $@"
     else
         # For debugging, set PSCALE_VERSION to a version of your choice. It defaults to "latest".
-        command="docker run -e PLANETSCALE_SERVICE_TOKEN=${PLANETSCALE_SERVICE_TOKEN:-""} -e PLANETSCALE_SERVICE_TOKEN_ID=$PLANETSCALE_SERVICE_TOKEN_ID -e PLANETSCALE_SERVICE_TOKEN_NAME=$PLANETSCALE_SERVICE_TOKEN_NAME -e HOME=/tmp -v $HOME/.config/planetscale:/tmp/.config/planetscale -e PSCALE_ALLOW_NONINTERACTIVE_SHELL=true --user $(id -u):$(id -g) --rm -i $tty planetscale/pscale:${PSCALE_VERSION:-"latest"} $@"
+        command="docker run -e PLANETSCALE_SERVICE_TOKEN=${PLANETSCALE_SERVICE_TOKEN:-""} -e PLANETSCALE_SERVICE_TOKEN_ID=$PLANETSCALE_SERVICE_TOKEN_ID -e PLANETSCALE_SERVICE_TOKEN_NAME=$PLANETSCALE_SERVICE_TOKEN_NAME -e HOME=/tmp -v $HOME/.config/planetscale:/tmp/.config/planetscale -v ${TMP_DIR:-/tmp/seed-data}:${TMP_DIR:-/tmp/seed-data}:ro -e PSCALE_ALLOW_NONINTERACTIVE_SHELL=true --user $(id -u):$(id -g) --rm -i $tty planetscale/pscale:${PSCALE_VERSION:-"latest"} $@"
     fi
 
     # if command is auth and we are running in CI, we will use the script command to get a fake terminal
