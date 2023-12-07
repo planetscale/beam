c431fdafe344de4cab7e98f5dcada65e702db014
Jesse Winton <jrandallwinton@gmail.com>
2023-12-06 23:43:24 -0500

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import '~/styles/globals.css'
 
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import { cookies } from 'next/headers'
 
 import { TRPCReactProvider } from '~/trpc/react'
@@ -13,11 +13,23 @@ import { getServerAuthSession } from '~/server/auth'
 import { SearchDialog } from './_components/search-dialog'
 import { AlertDialog } from './_components/alert-dialog'
 
-const inter = Inter({
-  subsets: ['latin'],
+const inter = localFont({
   variable: '--font-sans',
+  display: 'swap',
+  style: 'oblique 0deg 10deg',
+  src: [
+    {
+      path: '../../public/fonts/inter-roman.var.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/inter-italic.var.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+  ],
 })
-
 export const metadata = {
   title: 'Beam',
 }
