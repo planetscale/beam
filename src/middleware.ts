61028dc0f573b439385522e0473fb49598bc53d9
Jesse Winton <jrandallwinton@gmail.com>
2023-12-06 23:52:36 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|sign-in|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|sign-in|_next/static|_next/image|images|favicon.ico).*)',
   ],
 }
