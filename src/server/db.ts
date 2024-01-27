b913a29818df9ba4e5d1467e2ab3b1df4d82baed
Jesse Winton <jrandallwinton@gmail.com>
2024-01-26 21:09:08 -0500

    add driver and adapter to prisma

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,25 @@
 import { PrismaClient } from '@prisma/client'
 
 import { env } from '~/env.js'
+import { type Config, Client } from '@planetscale/database'
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
+
+const config = {
+  url: env.DATABASE_URL,
+} satisfies Config
+
+const client = new Client(config)
+
+const adapter = new PrismaPlanetScale(client)
+const prisma = new PrismaClient({
+  adapter,
+  log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+})
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  })
+export const db = prisma
 
 if (env.NODE_ENV !== 'production') globalForPrisma.prisma = db
