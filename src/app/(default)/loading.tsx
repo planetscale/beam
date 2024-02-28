f725b51585dd2a37befde60352bc63c36db3f7f6
Jesse Winton <jrandallwinton@gmail.com>
2024-02-28 12:35:08 -0800

    Create loading.tsx

---
+++ src/app/(default)/loading.tsx
@@ -0,0 +1,37 @@
+import { classNames } from '~/utils/core'
+
+export default function FeedLoading() {
+  const skeletons = Array.from({ length: 5 })
+
+  return (
+    <div className="divide-y divide-gray-700">
+      {skeletons.map((_, i) => (
+        <div className="animate-pulse py-8" key={i}>
+          <div className="w-3/4 h-8 bg-gray-200 rounded dark:bg-gray-700" />
+          <div
+            className={classNames(
+              'flex items-center justify-between gap-4 mt-6',
+            )}
+          >
+            <div className="flex items-center gap-4">
+              <div className="w-12 h-12 bg-gray-200 rounded-full dark:bg-gray-700" />
+
+              <div className="flex-1">
+                <div className="w-24 h-4 bg-gray-200 rounded dark:bg-gray-700" />
+
+                <div className="w-32 h-3 mt-2 bg-gray-200 rounded dark:bg-gray-700" />
+              </div>
+            </div>
+          </div>
+          <div className="space-y-3 mt-7">
+            <div className="grid grid-cols-3 gap-4">
+              <div className="h-5 col-span-2 bg-gray-200 rounded dark:bg-gray-700" />
+              <div className="h-5 col-span-1 bg-gray-200 rounded dark:bg-gray-700" />
+            </div>
+            <div className="w-1/2 h-5 bg-gray-200 rounded dark:bg-gray-700" />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
