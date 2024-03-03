969b6ab7fae2b640e70ff9d3144a8685362f2650
Jesse Winton <jrandallwinton@gmail.com>
2024-03-02 18:58:44 -0800

    updates

--- src/components/edit-post-form.tsx
+++ src/components/edit-post-form.tsx
@@ -1,23 +1,13 @@
 'use client'
 
 import * as React from 'react'
-import { Controller, useForm } from 'react-hook-form'
-import { TextField } from '~/components/text-field'
-import { Button } from '~/components/button'
-import MarkdownIcon from '~/components/svg/markdown-icon'
-import { MarkdownEditor } from '~/components/markdown-editor'
 
 import { api } from '~/trpc/react'
 import { useRouter } from 'next/navigation'
 import { markdownToHtml } from '~/utils/text'
 import { useSession } from 'next-auth/react'
 import { PostSkeleton } from './post-skeleton'
-import { useEffect } from 'react'
-
-type FormData = {
-  title: string
-  content: string
-}
+import { PostForm } from './post-form'
 
 type PostFormProps = {
   postId: number
@@ -26,28 +16,8 @@ type PostFormProps = {
 }
 
 export const EditPostForm = ({ postId, backTo }: PostFormProps) => {
-  const { data, mutate, isLoading } = usePostData(postId)
+  const { data, isLoading } = usePostData(postId)
   const { data: session } = useSession()
-
-  const { control, register, reset, handleSubmit } = useForm<FormData>({
-    defaultValues: {
-      title: data?.title,
-      content: data?.content,
-    },
-  })
-
-  const onSubmit = (data: FormData) => {
-    mutate({
-      id: postId,
-      data,
-    })
-  }
-
-  useEffect(() => {
-    if (data) {
-      reset(data)
-    }
-  }, [data])
 
   if (isLoading || !data) return <PostSkeleton />
 
@@ -62,58 +32,13 @@ export const EditPostForm = ({ postId, backTo }: PostFormProps) => {
         Edit &quot;{data?.title}&quot;
       </h1>
       <div className="mt-6">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-            {...register('title', { required: true })}
-            label="Title"
-            autoFocus
-            required
-            className="text-lg font-semibold !py-1.5"
-          />
-
-          <div className="mt-6">
-            <Controller
-              name="content"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <MarkdownEditor
-                  label="Post"
-                  value={field.value}
-                  onChange={field.onChange}
-                  onTriggerSubmit={handleSubmit(onSubmit)}
-                  required
-                />
-              )}
-            />
-          </div>
-
-          <div className="flex items-center justify-between gap-4 mt-8">
-            <div className="flex gap-4">
-              <Button
-                type="submit"
-                isLoading={isLoading}
-                loadingChildren={`${isLoading ? 'Saving' : 'Save'}`}
-              >
-                Save
-              </Button>
-              <Button href={backTo} variant="secondary">
-                Cancel
-              </Button>
-            </div>
-            {!isLoading && (
-              <a
-                href="https://docs.github.com/en/github/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax"
-                target="_blank"
-                rel="noreferrer"
-                className="flex items-center gap-2 transition-colors text-secondary hover:text-blue"
-              >
-                <MarkdownIcon />
-                <span className="text-xs">Markdown supported</span>
-              </a>
-            )}
-          </div>
-        </form>
+        <PostForm
+          defaultValues={{
+            title: data.title,
+            content: data.content,
+          }}
+          backTo={backTo}
+        />
       </div>
     </>
   )
