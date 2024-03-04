25e6e51f7a525ae6951544942a39753579fd8d64
Jesse Winton <jrandallwinton@gmail.com>
2024-03-04 11:51:12 -0800

    add redirect if not-authed

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -1,12 +1,20 @@
 import { type ReactNode } from 'react'
 import { Header } from '../_components/header'
 import { Footer } from '../_components/footer'
+import { getServerAuthSession } from '~/server/auth'
+import { redirect } from 'next/navigation'
 
 export default async function DefaultLayout({
   children,
 }: {
   children: ReactNode
 }) {
+  const session = await getServerAuthSession()
+
+  if (!session) {
+    redirect('/sign-in')
+  }
+
   return (
     <>
       <div className="max-w-3xl px-6 mx-auto">
